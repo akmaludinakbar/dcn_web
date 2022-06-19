31944dd9a42780a6fa1a33d4861bd2ebcab95806
Akmaludin akbar <akmaludin.akbar@tapera.go.id>
2022-06-19 22:10:26 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "build",
   env: {
     // BASE_URL:'http://localhost:8090' //dev
     BASE_URL: "http://api.dinarcahayanegara.com", //prod
